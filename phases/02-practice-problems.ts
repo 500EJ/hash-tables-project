079a3d0216015e7969b56504490c88ae6ad01352
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2023-02-04 14:11:55 -0500

    Initial commit

---
+++ phases/02-practice-problems.ts
@@ -0,0 +1,19 @@
+export function anagrams(str1, str2) {
+  // Your code here
+}
+
+export function commonElements(arr1, arr2) {
+  // Your code here
+}
+
+export function duplicate(arr) {
+  // Your code here
+}
+
+export function twoSum(nums, target) {
+  // Your code here
+}
+
+export function wordPattern(pattern, strings) {
+  // Your code here
+}
